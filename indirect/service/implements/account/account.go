15826cc3a69ec06e82eff528c186a985460eb0d2
imparting <1035758885@qq.com>
2023-07-29 19:02:46 +0800

    db查询

--- indirect/service/implements/account/account.go
+++ indirect/service/implements/account/account.go
@@ -11,16 +11,26 @@ type SAccount struct {
 }
 
 func (s *SAccount) Create(in dto.AccountCreateInDto) dto.AccountCreateOutDto {
+	// 判断表是否存在
+	if !db.HasTable(&model.User{}) {
+		if err := db.AutoMigrate(&model.User{}); err != nil {
+			panic("failed to create table")
+		}
+	}
+
 	user := model.User{
 		Username: in.Account,
 		Password: in.Password,
 	}
+
 	res := db.Table("user").Create(&user)
+	u := &model.User{}
+	db.Table("user").Where("id = ?", user.ID).First(u)
 	fmt.Println(res)
 
 	return dto.AccountCreateOutDto{
 		Account:  in.Account,
 		Password: in.Password,
-		Count:    int64(user.ID),
+		Count:    int64(u.ID),
 	}
 }
